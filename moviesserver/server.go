11dc09ab0d0cbeb3431ef8071f81d7d2a09a526d
Nate Rivera <natanael.j.rivera@gmail.com>
2018-05-06 18:02:58 -0400

    made server

---
+++ moviesserver/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/movies", getMoviesHandler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func getMoviesHandler(w http.ResponseWriter, r *http.Request) {
+	m := Movie{Title: "Memento", Director: "Christopher Nolan"}
+	jsonMovie, _ := json.Marshal(m)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonMovie)
+}
+
+type Movie struct {
+	Title    string `json:"title"`
+	Director string `json:"director"`
+}
